19353c77153fc929260b85c8b0ed244694a8b7d6
bbbbmo <bbbbmo@github.com>
2025-12-24 08:54:41 +0900

    💄feat: shacdn 컴포넌트 추가 및 레이아웃 생성

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="h-screen w-full">{children}</body>
+    </html>
+  );
+}
