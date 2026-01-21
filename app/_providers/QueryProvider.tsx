c2a3bfc94924905dba32c2a89b318bc8894036fb
bbbbmo <aqw20501@naver.com>
2026-01-21 22:31:00 +0900

    🔨chore: QueryClient 옵션 수정

--- app/_providers/QueryProvider.tsx
+++ app/_providers/QueryProvider.tsx
@@ -13,8 +13,9 @@ export default function QueryProvider({
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 1분
+            staleTime: 5 * 60 * 1000, // 5분
             refetchOnWindowFocus: false,
+            retry: false,
           },
         },
       })
