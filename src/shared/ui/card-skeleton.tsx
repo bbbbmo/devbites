b339ef9d50ca7331124437cb52b52096bc3e7464
bbbbmo <aqw20501@naver.com>
2026-01-27 22:56:42 +0900

    💄style: 스켈레톤 추가

---
+++ src/shared/ui/card-skeleton.tsx
@@ -0,0 +1,19 @@
+import { cn } from "../lib/utils";
+import { Card, CardContent, CardHeader } from "./card";
+import { Skeleton } from "./skeleton";
+
+function CardSkeleton({ className }: { className: string }) {
+  return (
+    <Card className={cn("animate-pulse rounded-md", className)}>
+      <CardHeader>
+        <Skeleton className="h-4 w-2/3 block" />
+        <Skeleton className="h-4 w-1/2 block" />
+      </CardHeader>
+      <CardContent className="flex-1 w-full overflow-hidden flex flex-col min-h-0">
+        <Skeleton className="block w-full" />
+      </CardContent>
+    </Card>
+  );
+}
+
+export { CardSkeleton };
